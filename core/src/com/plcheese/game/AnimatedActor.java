1e16209888f10db9b4235ed58c36a85b0e70b870
mickey1cx <mickey1cx@gmail.com>
2018-04-01 20:48:43 +0300

    mouse animation

---
+++ core/src/com/plcheese/game/AnimatedActor.java
@@ -0,0 +1,48 @@
+package com.plcheese.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+
+public class AnimatedActor extends BaseActor {
+
+    private Animation<TextureRegion> anim;
+    public float elapsedTime;
+
+
+    public AnimatedActor() {
+
+        super();
+        elapsedTime = 0;
+
+    }
+
+    public void setAnimimation(Animation<TextureRegion> a) {
+
+        setTexture(a.getKeyFrame(0).getTexture());
+        anim = a;
+    }
+
+    @Override
+    public void act(float dt) {
+
+        super.act(dt);
+
+        elapsedTime += dt;
+
+        if(velocityX !=0 || velocityY !=0) {
+            setRotation(MathUtils.atan2(velocityY, velocityX) * MathUtils.radiansToDegrees);
+        }
+
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+
+        region.setRegion(anim.getKeyFrame(elapsedTime));
+        super.draw(batch, parentAlpha);
+
+    }
+}
--- core/src/com/plcheese/game/PleaseCheese.java
+++ core/src/com/plcheese/game/PleaseCheese.java
@@ -8,16 +8,20 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.utils.Array;
 
 public class PleaseCheese extends Game {
 
 	public Stage mainStage;
-	private BaseActor mousey;
+	private AnimatedActor mousey;
 	private BaseActor cheese;
 	private BaseActor floor;
 	private BaseActor winText;
@@ -26,23 +30,41 @@ public class PleaseCheese extends Game {
 	private Action spinShrinkFadeOut;
 	private Action fadeInColorCycleForever;
 
+	private Animation mouseAnim;
+
 	@Override
 	public void create () {
 
 		mainStage = new Stage();
 
-		floor = createActor("tiles.jpg", 0, 0);
+		initActions();
+		initAnimation();
+
+		floor = createActor("tiles.jpg", Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() /2);
 		cheese = createActor("cheese.png", 400, 300);
-		mousey = createActor("mouse.png", 20, 20);
-		winText = createActor("you-win.png", 170, 60, false);
+		mousey = createAnimetedActor(mouseAnim, 20, 20);
+		winText = createActor("you-win.png", Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() /2, false);
 
 		win = false;
 
-		createActions();
 
 	}
 
-	private void createActions() {
+	private void initAnimation() {
+
+		TextureRegion[] frames = new TextureRegion[4];
+		for (int i = 0; i < 4; i++) {
+			String fileName = "mouse_" + i + ".png";
+			Texture tex = new Texture(Gdx.files.internal(fileName));
+			tex.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+			frames[i] = new TextureRegion(tex);
+		}
+		Array<TextureRegion> framesArray = new Array<TextureRegion>(frames);
+		mouseAnim = new Animation(0.1f, framesArray, Animation.PlayMode.LOOP_PINGPONG);
+
+	}
+
+	private void initActions() {
 
 		spinShrinkFadeOut = Actions.parallel(
 				Actions.alpha(1),
@@ -57,8 +79,8 @@ public class PleaseCheese extends Game {
 				Actions.fadeIn(2),
 				Actions.forever(
 						Actions.sequence(
-								Actions.color(new Color(1, 0, 0, 1), 1),
-								Actions.color(new Color(1, 0, 1, 1), 1)
+								Actions.color(new Color(0.5f, 0.5f, 0.5f, 1), 1),
+								Actions.color(new Color(1, 1, 1, 1), 1)
 						)
 				)
 		);
@@ -75,8 +97,20 @@ public class PleaseCheese extends Game {
 		actor.setTexture(new Texture(
 				Gdx.files.internal(pathToTexture)
 		));
-		actor.setPosition(x,y);
+		actor.setOrigin(actor.getWidth() / 2, actor.getHeight() / 2);
+		actor.setPosition(x - actor.getOriginX(),y - actor.getOriginY());
 		actor.setVisible(visible);
+		mainStage.addActor(actor);
+
+		return actor;
+
+	}
+
+	private AnimatedActor createAnimetedActor(Animation animation, float x, float y) {
+
+		AnimatedActor actor = new AnimatedActor();
+		actor.setAnimimation(animation);
+		actor.setPosition(x,y);
 		actor.setOrigin(actor.getWidth() / 2, actor.getHeight() / 2);
 		mainStage.addActor(actor);
 

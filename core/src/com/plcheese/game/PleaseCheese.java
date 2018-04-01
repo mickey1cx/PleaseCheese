d7f79cbe3c2c5ebe5e4c914be025f482537d0685
mickey1cx <mickey1cx@gmail.com>
2018-04-01 19:39:51 +0300

    Initial commit

---
+++ core/src/com/plcheese/game/PleaseCheese.java
@@ -0,0 +1,85 @@
+package com.plcheese.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class PleaseCheese extends Game {
+
+	public Stage mainStage;
+	private BaseActor mousey;
+	private BaseActor cheese;
+	private BaseActor floor;
+	private BaseActor winText;
+
+	@Override
+	public void create () {
+
+		mainStage = new Stage();
+
+		floor = createActor("tiles.jpg", 0, 0);
+		cheese = createActor("cheese.png", 400, 300);
+		mousey = createActor("mouse.png", 20, 20);
+		winText = createActor("you-win.png", 170, 60, false);
+
+	}
+
+	private BaseActor createActor(String pathToTexture, float x, float y) {
+		return createActor(pathToTexture, x, y, true);
+	}
+
+	private BaseActor createActor(String pathToTexture, float x, float y, boolean visible) {
+
+		BaseActor actor = new BaseActor();
+		actor.setTexture(new Texture(
+				Gdx.files.internal(pathToTexture)
+		));
+		actor.setPosition(x,y);
+		actor.setVisible(visible);
+		mainStage.addActor(actor);
+
+		return actor;
+
+	}
+
+	@Override
+	public void render () {
+
+		mousey.velocityY = 0;
+		mousey.velocityX = 0;
+
+		if (Gdx.input.isKeyPressed(Keys.LEFT)) mousey.velocityX -= 100;
+		if (Gdx.input.isKeyPressed(Keys.RIGHT)) mousey.velocityX += 100;
+		if (Gdx.input.isKeyPressed(Keys.UP)) mousey.velocityY += 100;
+		if (Gdx.input.isKeyPressed(Keys.DOWN)) mousey.velocityY -= 100;
+
+		float dt = Gdx.graphics.getDeltaTime();
+		mainStage.act(dt);
+
+		Rectangle cheeseRect = cheese.getBoundingRectangle();
+		Rectangle mouseyRect = mousey.getBoundingRectangle();
+
+
+		if (cheeseRect.contains(mouseyRect)) {
+			winText.setVisible(true);
+		}
+
+		Gdx.gl.glClearColor(0.8f, 0.8f, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		mainStage.draw();
+
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
